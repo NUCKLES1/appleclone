c3bc6b9778dcbcd8453e49a4b6daa6309806218a
Nuckles1 <danyxcharge@gmail.com>
2024-04-03 12:00:40 -0700

    commit

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -37,7 +37,7 @@ const HowItWorks = () => {
         <div className="flex flex-col items-center">
           <h2 className="hiw-title">
             A17 Pro chip.
-            <br /> A monster win for gaming..
+            <br /> A monster win for gaming.
           </h2>
 
           <p className="hiw-subtitle">
